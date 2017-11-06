b6097d8338ce1a9808b87e44e850fa078a3f7a94
Juju C <juliette.coustard@gmail.com>
2017-11-06 16:50:48 +0100

    JS: stored data in localStorage

--- script.js
+++ script.js
@@ -4,22 +4,41 @@ const $items = $('.items');
 const $delete = $('.item-delete');
 let count = $items.children().length;
 
+const items = loadStorage();
+//items.forEach(addItemToList);
+for (let item of items) {
+    addItemToList(item);
+}
+
 // -------------------------- Events -------------------------
 $form.submit( function(e) {
     e.preventDefault();
 
     const value = $field.val();
-    if(value) addItem(value);
+    if(value) addItemToList(value);
+    addToStorage(value);
     $field.val('');
 });
 
 $('.items').on('click', '.item-delete', function(e) {
-    const item = $(e.target).parent()[0];
+    const item = $(e.target).parent();
+
     deleteItem(item);
 });
 
 // ------------------------ Functions ------------------------
-function addItem(value) {
+function loadStorage() {
+    try {
+        console.log(localStorage.items);
+        return JSON.parse(localStorage.items) || [];
+    }
+    catch(err) {
+        console.warn('Error while loading data: ', err);
+        return [];
+    }
+}
+
+function addItemToList(value) {
     let i = count++;
     $('.items')
         .append(`
@@ -31,6 +50,21 @@ function addItem(value) {
         `);
 };
 
+function addToStorage(value) {
+    items.push(value);
+    localStorage.items = JSON.stringify(items);
+}
+
 function deleteItem(item) {
-    if(confirm('Remove this item?'),item.remove());
+    if(confirm('Remove this item?')) {
+        const index = item.index();
+        removeFromStorage(index);
+        item.remove();
+    }
+}
+
+function removeFromStorage(index) {
+    console.log('index', index);
+    items.splice(index, 1);
+    localStorage.items = JSON.stringify(items);
 }
